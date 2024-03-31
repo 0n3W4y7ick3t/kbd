3a899077a65a01f1512326fc7a23562f981f253f
Leon <leonsatoshi39@gmail.com>
2024-03-31 18:17:09 +0900

    add crkbd-v4

---
+++ crkbd-v4/keymaps/v4_miryoku/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = no       # Mouse keys
+VIA_ENABLE      = false    # disable VIA
+LTO_ENABLE      = yes
+CAPS_WORD_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+ENCODER_MAP_ENABLE = no    # I dont use encoder
+BOOTLOADER = rp2040
